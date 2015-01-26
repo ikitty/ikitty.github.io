8a1f36e440aeea59fe175e6df4f7aa306e89cc75
ikitty <xkeitty@163.com>
2015-01-26 15:55:29 +0800

    up

--- labs/gulpfile.js
+++ labs/gulpfile.js
@@ -6,20 +6,22 @@
  * LastModify: 2014-12-09
  */
 
-var act_name = 'a20141212tmz';
+var act_name = 'a20150129test';
 
 // src dir , build dir
-var path_src = 'act_dev/' + act_name + '/'
-    ,path_target = 'act/' + act_name + '/'
-    ,path_template = 'act_template/'
+var workdir = '../t7/';
+
+var path_src = workdir + 'act_dev/' + act_name + '/'
+    ,path_target = workdir + 'act/' + act_name + '/'
+    ,path_template = workdir + 'act_template/'
     ,img_prefix = 'http://ossweb-img.qq.com/images/t7/act/' + act_name + '/'
     ;
 
 // plugin
 var gulp = require('gulp')
-	,del = require('del')
+    ,clean = require('gulp-clean')
     ,concat = require('gulp-concat')
-	,replace = require('gulp-replace')
+	,g_replace = require('gulp-replace')
 	,cssmin = require('gulp-minify-css')
 	,uglify = require('gulp-uglify')
     ,encode = require('gulp-convert-encoding')
@@ -59,17 +61,18 @@ gulp.task('initMobile',function(){
 gulp.task('css', function() {
     gulp.src(path_dev.css)
         .pipe(concat('style.css'))
-        .pipe(replace('(images/', '(' + img_prefix ))
+        .pipe(g_replace('(images/', '(' + img_prefix ))
         .pipe(cssmin({keepBreaks:true, noAdvanced: true, compatibility:'ie6'}))
         .pipe(gulp.dest(path_build.css))
 });
 
 // img
 gulp.task('img', function() {
-    del(path_build.img + '*', function () {
-        gulp.src(path_dev.img)
-            .pipe(gulp.dest(path_build.img))
-    });
+    gulp.src(path_build.img)
+        .pipe(clean({force: true}));
+        
+    gulp.src(path_dev.img)
+        .pipe(gulp.dest(path_build.img));
 });
 
 // js
@@ -84,31 +87,35 @@ gulp.task('js', function() {
 
 // html
 gulp.task('html',function(){
-	del([path_build.html + '*.htm', path_build.html + '/*.shtml'], function(){
-  		gulp.src(path_dev.html)
-            //如果开发文件是gbk编码，则先要转码后再操作，最后再转成gbk
-            //.pipe(encode({ from: 'gbk', to: 'utf-8'}))
-            .pipe(replace('charset="utf-8"', 'charset="gbk"')) 
-            .pipe(replace('src="images/', 'src="' + img_prefix ))
-            .pipe(replace('<script src="js/common.js"></script>', ''))
-            .pipe(encode({ to: 'gbk'}))
-            .pipe(gulp.dest(path_build.html))
+    gulp.src([path_build.html + '*.htm', path_build.html + '*.shtml'] )
+        .pipe(clean({force: true}));
 
-            console.log('Build all file. OK!')
-  	});
+    gulp.src(path_dev.html)
+        //如果开发文件是gbk编码，则先要转码后再操作，最后再转成gbk
+        //.pipe(encode({ from: 'gbk', to: 'utf-8'}))
+        .pipe(g_replace('charset="utf-8"', 'charset="gbk"')) 
+        .pipe(g_replace('src="images/', 'src="' + img_prefix ))
+        .pipe(g_replace('<script src="js/common.js"></script>', ''))
+        .pipe(encode({ to: 'gbk'}))
+        .pipe(gulp.dest(path_build.html))
+
+        var D = new Date();
+        console.log('Builded html. ==============================  At: ' +   D.getHours() + ':' + D.getMinutes() + ':' + D.getSeconds());
 });
 
 
-gulp.task('build', ['css', 'js', 'img', 'html'])
+gulp.task('build', ['css', 'js', 'img', 'html']);
 
-gulp.task('watch', function() {
+gulp.task('watch', ['css', 'js', 'img', 'html'], function() {
     gulp.watch(path_dev.js, ['js']);
     gulp.watch(path_dev.css, ['css']);
     gulp.watch(path_dev.img, ['img']);
     gulp.watch(path_dev.html, ['html']);
 });
 
-//common test
+// ====================================
+// common test
+// ====================================
 var rename = require('gulp-rename');
 gulp.task('jsmin',  function() {
     return gulp.src(path_template + '*.js')
