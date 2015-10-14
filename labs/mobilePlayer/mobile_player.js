dd1d553be4fbffb022b59b3e8e7ec2b360be0fef
ikitty <xkeitty@163.com>
2015-10-14 11:13:44 +0800

    update mobile player

--- labs/mobilePlayer/mobile_player.js
+++ labs/mobilePlayer/mobile_player.js
@@ -1,19 +1,9 @@
-var player;
-
-var loading ={
-    dom0:'<div class="sk-fading-circle"><div class="sk-circle1 sk-circle"></div><div class="sk-circle2 sk-circle"></div><div class="sk-circle3 sk-circle"></div><div class="sk-circle4 sk-circle"></div><div class="sk-circle5 sk-circle"></div><div class="sk-circle6 sk-circle"></div><div class="sk-circle7 sk-circle"></div><div class="sk-circle8 sk-circle"></div><div class="sk-circle9 sk-circle"></div><div class="sk-circle10 sk-circle"></div><div class="sk-circle11 sk-circle"></div><div class="sk-circle12 sk-circle"></div></div>',
-    dom:'Loading',
-    end:'<div id="mCoverImg"></div>'
-}
-
-function parseTime(t){
-    t = Math.floor(t);
-    var m = Math.floor(t/60);
-    var s = (t%60)<10?'0'+t%60:t%60;
-    return m+":"+s;
-}
-
-/* =====start===== */
+/**
+ * mobile player for mobile device
+ *
+ * @param {Object} {'songlist': [songInfo]}
+ *
+ **/
 var MobilePlayer = function () {
     this.init.apply(this, arguments);
 };  
@@ -22,7 +12,6 @@ MobilePlayer.prototype = {
         //self config
         this.isBegin = false;
         this.isPlaying = false ;
-        this.curMusicPos = 0 ;
 
         this.playPause = $('#mPlayPause');
 
@@ -55,28 +44,22 @@ MobilePlayer.prototype = {
 
         //init DOM
         $(songlist).each(function (i, k) {
-            $('#mpList').append('<li>' + k.n + ' &nbsp; '+ k.a + '</li>');
+            $('#mpList').append('<li>' + k.n + '<span>'+ k.a + '</span></li>');
         });
         $('#mpList li').tap(function () {
             $.qPlayer.playAnyPos($(this).index());
-            self.showSongInfo();
+            self.resetSongUI();
 
             $('#plistWrap').fadeOut();
         });
-        $('#btnList').tap(function () {
-            $('#plistWrap').fadeIn();
+        $('#showList').tap(function () {
+            $('#plistWrap').fadeIn(300);
         });
         $('#plistClose').tap(function () {
-            $('#plistWrap').fadeOut();
+            $('#plistWrap').fadeOut(200);
         });
-
-        $('#mpList li').attr('class','');
-        $('#mpList li').eq($.qPlayer.playList.getPostion()).addClass('nowPlaying');
-
     }
     ,initUI: function () {
-        // $('#mTitle').html(song.n+'<span>'+song.a+'</span>');
-        
         var self = this;
         this.playPause.tap(function () {
             if (self.isPlaying) {
@@ -93,55 +76,55 @@ MobilePlayer.prototype = {
         });
     }
     ,initQPlayer: function () {
+        function parseTime(t){
+            t = Math.floor(t);
+            var m = Math.floor(t/60);
+            var s = (t%60)<10?'0'+t%60:t%60;
+            return m+":"+s;
+        }
+
         $.qPlayer.on("timeupdate", function (timeinfo) {
-            $('#mCur').html(parseTime(timeinfo.currentTime));
-            $('#mTol').html(parseTime(timeinfo.totalTime));
-            $('#mPlayed').css('width',timeinfo.currentTime * 100 / timeinfo.totalTime + "%");
+            $('#songTimeCur').html(parseTime(timeinfo.currentTime));
+            $('#songTimeTotal').html(parseTime(timeinfo.totalTime));
+            $('#songPlayed').css('width',timeinfo.currentTime * 100 / timeinfo.totalTime + "%");
         });
         $.qPlayer.on("downloadprogress", function (s) {
-            $('#mDownload').width(s.nProgress + "%");
+            $('#songDown').width(s.nProgress + "%");
         });
-
-        //todo在 qp状态变更的同时切换所有状态
     }
     ,play: function () {
         $.qPlayer.play();
         this.isPlaying = 1 ;
-        this.playPause.removeClass('mPlay').addClass('mPause');
-        this.showSongInfo();
+        this.resetSongUI();
     }
     ,pause: function () {
         $.qPlayer.pause();
         this.isPlaying = 0 ;
-        this.playPause.removeClass('mPause').addClass('mPlay');
-        $('.mCoverBack').addClass('aniPause');
+        this.playPause.removeClass('song_pause').addClass('song_play');
+        $('#cdEdge').addClass('aniPause');
     }
     ,prev: function () {
         $.qPlayer.prev(); 
         this.isPlaying = 1 ;
-        this.playPause.removeClass('mPlay').addClass('mPause');
-        this.showSongInfo();
-        $('#mDownload').width("0%");
-        $('#mPlayed').width('0%');
-        $('#mCur').html(0);
-        $('#mTol').html(0);
+        this.resetSongUI();
     }
     ,next: function () {
         $.qPlayer.next(); 
         this.isPlaying = 1 ;
-        this.playPause.removeClass('mPlay').addClass('mPause');
-        this.showSongInfo();
-        $('#mDownload').width("0%");
-        $('#mPlayed').width('0%');
-        $('#mCur').html(0);
-        $('#mTol').html(0);
+        this.resetSongUI();
     }
-    ,showSongInfo: function () {
+    ,resetSongUI: function () {
         var song = $.qPlayer.playList.getSongInfoObj();
 
         $('#mTitle').html(song.n+'<span>'+song.a+'</span>');
-        $('#mbg , #mCoverImg').attr('style','background-image:url(http://game.gtimg.cn/images/mho/act/a20150716music/cover/bg'+song.id+'.jpg');
-        $('.mCoverBack').addClass('aniRotate').removeClass('aniPause');
+        $('#mBg , #cdImg').attr('style','background-image:url(http://game.gtimg.cn/images/mho/act/a20150716music/cover/bg'+song.id+'.jpg)');
+
+        $('#cdEdge').addClass('aniRotate').removeClass('aniPause');
+        this.playPause.removeClass('song_play').addClass('song_pause');
+        $('#songDown, #songPlayed').width("0%");
+        $('#songTimeCur, #songTimeTotal').html('00:00');
+
+        $('#mpList li').removeClass().eq( $.qPlayer.playList.getPostion() ).addClass('nowPlaying');
     }
 }
 
